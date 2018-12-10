703fa63eed7519b1a6e7b951056ebf7c81a4f938
Mikael Löfstrand <mikael.lofstrand@trioptima.com>
2018-12-10 18:47:42 +0100

    start of day 8

---
+++ src/Day8.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+
+use Illuminate\Support\Collection;
+
+class Day8
+{
+    public static function sum($input)
+    {
+        $a = new Collection(self::parseInput($input));
+
+        $children = new Collection([]);
+        $meta = new Collection([]);
+
+        while ($a->count() > 0) {
+            $readNewNode = true;
+            if ($children->count() > 0) {
+                // keep reading children...
+                $parent = $children->pop();
+                if ($parent['children'] == 0) {
+                    $noChildren = 0;
+                    $noMeta = $parent['metadata'];
+//                    echo "Read parent node " . $noChildren . ':' . $noMeta . "\n";
+                    $readNewNode = false;
+                } else {
+//                    echo "Keeping parent node for later " . ($parent['children'] - 1) . ':' . $parent['metadata'] . "\n";
+                    $children->push(['children' => ($parent['children'] - 1), 'metadata' => $parent['metadata']]);
+                }
+            }
+
+            if ($readNewNode) {
+                // Read new "parent"
+                $noChildren = $a->shift();
+                $noMeta = $a->shift();
+//                echo "Read node " . $noChildren . ':' . $noMeta . "\n";
+            }
+
+            if ($noChildren == 0) {
+                // Read metadata directly
+//                echo "Saving metadata for node: ";
+                for ($n = 0; $n < $noMeta; $n++) {
+                    $data = $a->shift();
+//                    echo "$data ";
+                    $meta->push($data);
+                }
+//                echo "\n";
+            } else {
+//                echo "Saving parent node for later " . $noChildren . ':' . $noMeta . "\n";
+                $children->push(['children' => $noChildren, 'metadata' => $noMeta]);
+            }
+
+        }
+
+        $sum = $meta->sum();
+
+
+        return $sum;
+    }
+
+    public static function parseInput($input)
+    {
+        if (is_array($input)) {
+            $input = trim(join('', $input));
+        }
+
+        $out = explode(' ', $input);
+        return $out;
+    }
+}
---
+++ tests/Day8Test.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Day6;
+use App\Day7;
+use App\Day8;
+use PHPUnit\Framework\TestCase;
+
+final class Day8Test extends TestCase
+{
+    /**
+     * @dataProvider rootSumProvider
+     */
+    public function testRootSum($data, $expected)
+    {
+        $result = Day8::sum($data);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @dataProvider sumProvider
+     */
+    public function testSum($data, $expected)
+    {
+        $result = Day8::sum($data);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function sumProvider()
+    {
+        return [
+            [
+                "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2",
+                138,
+            ],
+        ];
+    }
+
+    public function rootSumProvider()
+    {
+        return [
+            [
+                "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2",
+                66,
+            ],
+        ];
+    }
+}
