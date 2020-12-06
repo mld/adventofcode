b790c5febd16cfc220edb8a18d36881bda135f2c
Mikael Löfstrand <micke@lofstrand.net>
2020-12-06 12:44:47 +0100

    day 6

--- src/Command/Day06Part1Command.php
+++ src/Command/Day06Part1Command.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Day06\CustomsDeclarationForms;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -20,7 +21,8 @@ class Day06Part1Command extends FileInputCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $contents = $this->parseFiles($input->getArgument('filename'));
-
+        $cdf = new CustomsDeclarationForms($contents);
+        $output->writeln('Sum of distinct yes per group: ' . $cdf->sumDistinctYesPerGroup());
         return 0;
     }
 }
---
+++ src/Day06/CustomsDeclarationForms.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Day06;
+
+class CustomsDeclarationForms
+{
+    /** @var array<mixed> */
+    protected $list;
+
+    /**
+     * Passports constructor.
+     * @param array<string> $rows
+     */
+    public function __construct(array $rows)
+    {
+        $this->parseInput($rows);
+    }
+
+    /**
+     * @param array<string> $rows
+     */
+    public function parseInput(array $rows): void
+    {
+        $this->list = [];
+        $raw = join('', $rows);
+        $parts = explode("\n\n", $raw);
+        printf("Found %d groups\n", count($parts));
+        $this->list = $parts;
+    }
+
+    public function countDistinctYesInGroup(string $input): int
+    {
+        $entry = preg_replace('/\s+/', '', $input);
+        if ($entry === false) {
+            return 0;
+        }
+        if (strlen($entry) == 0) {
+            return 0;
+        }
+
+        $count = count_chars($entry, 1);
+        return count($count);
+    }
+
+    public function countUnanimousYesInGroup(string $input): int
+    {
+        $input = explode("\n", $input);
+        $work = null;
+        foreach ($input as $entry) {
+            $entry = preg_replace('/\s+/', '', $entry);
+            if ($entry === false) {
+                continue;
+            }
+            if (strlen($entry) == 0) {
+                continue;
+            }
+
+            $entryCount = count_chars($entry, 1);
+            if ($work === null) {
+                $work = $entryCount;
+            }
+            $work = array_intersect_key($work, $entryCount);
+        }
+        return count($work);
+    }
+
+    public function sumDistinctYesPerGroup(): int
+    {
+        $sum = 0;
+        foreach ($this->list as $entry) {
+            $sum += $this->countDistinctYesInGroup($entry);
+        }
+        return $sum;
+    }
+
+    public function sumUnanimousYesPerGroup(): int
+    {
+        $sum = 0;
+        foreach ($this->list as $entry) {
+            $sum += $this->countUnanimousYesInGroup($entry);
+        }
+        return $sum;
+    }
+}
